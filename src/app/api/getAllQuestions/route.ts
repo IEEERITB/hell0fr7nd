5836b4c5b4ded18c99dacbdd9b6e97fb151da9d9
TheShiveshNetwork <ieeeritb@gmail.com>
2025-01-02 00:06:25 +0530

    fixed build errors for deployment

--- src/app/api/getAllQuestions/route.ts
+++ src/app/api/getAllQuestions/route.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const QUESTION_FILE = "./src/db/questions.json";
 const QUESTION_FILE_PATH = path.join(process.cwd(), QUESTION_FILE);
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
     const fileRes = await fsPromises.readFile(QUESTION_FILE_PATH, "utf-8");
     if (!fileRes) return NextResponse.json({ status: 500, message: "Internal Server Error" });
     const allQuestions = JSON.parse(fileRes);
